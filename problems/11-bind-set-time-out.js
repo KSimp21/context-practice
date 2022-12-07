66ef7b8f365b443291611efb0adf1b7dfdbc5a11
Kish <kisha_simpson@yahoo.com>
2022-12-06 23:16:04 -0600

    number 11

--- problems/11-bind-set-time-out.js
+++ problems/11-bind-set-time-out.js
@@ -1,6 +1,12 @@
 function boundFuncTimer(obj, func, delay) {
-  // Your code here
+
+  setTimeout(() => {
+    func.call(obj)
+  }, delay)
+
 }
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
