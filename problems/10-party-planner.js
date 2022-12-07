da3aa93673ddc3a80868cd258d44301c2b2ea2d2
jacob <jaycee12dg2s@gmail.com>
2022-12-06 22:12:34 -0700

    10 solved

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,4 +1,23 @@
-// Your code here
+class PartyPlanner {
+	constructor(guestList) {
+		this.guestList = [];
+	}
+	addToGuestList(name) {
+		return this.guestList.push(name)
+	}
+	throwParty () {
+		if(this.guestList.length > 0) {
+			let string = `Welcome to the party ${this.guestList[0]}`
+			for(let i = 1; i < this.guestList.length; i++) {
+				string += ` and ${this.guestList[i]}`
+			}
+			return string
+		}
+		else {
+			return "Gotta add people to the guest list"
+		}
+	}
+}
 
 
 /*****************************************************************************/
